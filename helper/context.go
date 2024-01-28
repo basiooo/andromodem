38614a95398973a0cad52489c99217977cc2406b
Bagas Julianto <bagasjulianto212@gmail.com>
2024-01-28 17:39:40 +0700

    fix staticlint

--- helper/context.go
+++ helper/context.go
@@ -3,12 +3,13 @@ package helper
 import (
 	"errors"
 	adb "github.com/abccyz/goadb"
+	"github.com/basiooo/andromodem/middleware"
 	"net/http"
 )
 
 func GetADBClient(r *http.Request) (*adb.Adb, error) {
 	ctx := r.Context()
-	adbClient, ok := ctx.Value("adbClient").(*adb.Adb)
+	adbClient, ok := ctx.Value(middleware.AdbClientKey).(*adb.Adb)
 	if !ok {
 		return nil, errors.New("unable to retrieve adbClient")
 	}
--- middleware/AdbClient.go
+++ middleware/AdbClient.go
@@ -6,10 +6,14 @@ import (
 	"net/http"
 )
 
+type contextKey string
+
+const AdbClientKey contextKey = "adbClient"
+
 func AdbClient(adbClient *adb.Adb) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-			ctx := context.WithValue(request.Context(), "adbClient", adbClient)
+			ctx := context.WithValue(request.Context(), AdbClientKey, adbClient)
 			next.ServeHTTP(writer, request.WithContext(ctx))
 		})
 	}
