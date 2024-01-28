e8767521cc0b431afe75ad2f6698eaf440c62d62
Bagas Julianto <bagasjulianto212@gmail.com>
2024-01-28 20:23:29 +0700

    fix staticlint

--- middleware/AdbClient.go
+++ middleware/AdbClient.go
@@ -6,14 +6,11 @@ import (
 	"net/http"
 )
 
-type contextKey string
-
-const AdbClientKey contextKey = "adbClient"
-
 func AdbClient(adbClient *adb.Adb) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-			ctx := context.WithValue(request.Context(), AdbClientKey, adbClient)
+			//lint:ignore SA1029 Using string as map key is allowed in this case.
+			ctx := context.WithValue(request.Context(), "adbClient", adbClient)
 			next.ServeHTTP(writer, request.WithContext(ctx))
 		})
 	}
