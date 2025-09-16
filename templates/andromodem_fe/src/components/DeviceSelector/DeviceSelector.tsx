625b5e5bbf5723cca4524e0c8606bc4fccbd281d
Bagas Julianto <bagasjulianto212@gmail.com>
2025-09-16 22:25:22 +0700

    feat(DeviceSelector): add auto-select device when open Andromodem

--- templates/andromodem_fe/src/components/DeviceSelector/DeviceSelector.tsx
+++ templates/andromodem_fe/src/components/DeviceSelector/DeviceSelector.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react"
-import { useState } from "react"
+import { useEffect,useState } from "react"
 
 import useListenDevices from "@/hooks/useListenDevices"
 import { useDevicesStore } from "@/stores/devicesStore"
@@ -7,9 +7,44 @@ import { DeviceState } from "@/types/device"
 
 const DeviceSelector: FC = () => {
     const { devices, isConnected } = useListenDevices()
-    const { setDeviceUsed, unsetDeviceUsed } = useDevicesStore()
+    const { setDeviceUsed, deviceUsed, unsetDeviceUsed } = useDevicesStore()
 
     const [selectedDevice, setSelectedDevice] = useState<string>('DEFAULT')
+    const [selectedDeviceSerial, setSelectedDeviceSerial] = useState<string | null>(null)
+
+    const autoSelectEnabled = selectedDeviceSerial === deviceUsed?.serial
+
+    useEffect(() => {
+        const savedDeviceSerial = localStorage.getItem('selectedDeviceSerial')
+        setSelectedDeviceSerial(savedDeviceSerial)
+        if (savedDeviceSerial && savedDeviceSerial !== 'DEFAULT') {
+            setSelectedDevice(savedDeviceSerial)
+        }
+    }, [])
+
+    useEffect(() => {
+        const savedDeviceSerial = localStorage.getItem('selectedDeviceSerial')
+        setSelectedDeviceSerial(savedDeviceSerial)
+    }, [selectedDevice, deviceUsed])
+
+    useEffect(() => {
+        if (devices.length > 0 && isConnected) {
+            const savedDeviceSerial = localStorage.getItem('selectedDeviceSerial')
+
+            if (savedDeviceSerial && savedDeviceSerial !== 'DEFAULT') {
+                const savedDevice = devices.find(device =>
+                    device.serial === savedDeviceSerial &&
+                    device.state === DeviceState.ONLINE
+                )
+
+                if (savedDevice) {
+                    setSelectedDevice(savedDeviceSerial)
+                    setDeviceUsed(savedDevice)
+                    setSelectedDeviceSerial(savedDeviceSerial)
+                }
+            }
+        }
+    }, [devices, isConnected, setDeviceUsed])
 
     const handleDeviceSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const serial = e.target.value
@@ -22,11 +57,21 @@ const DeviceSelector: FC = () => {
             unsetDeviceUsed()
         }
     }
-    
+
+    const handleAutoSelectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) {
+            localStorage.setItem('selectedDeviceSerial', selectedDevice)
+        } else {
+            localStorage.removeItem('selectedDeviceSerial')
+        }
+        const savedDeviceSerial = localStorage.getItem('selectedDeviceSerial')
+        setSelectedDeviceSerial(savedDeviceSerial)
+    }
+
     return (
         <div className="card card-compact w-full bg-base-200 shadow-xl">
             <div className="card-body">
-                <h2 className="card-title text-md md:text-xl">Select Device 
+                <h2 className="card-title text-md md:text-xl">Select Device
                     {isConnected ? (
                         <div className="tooltip" data-tip="Server Connected">
                             <div aria-label="success" className="status status-success"></div>
@@ -43,7 +88,7 @@ const DeviceSelector: FC = () => {
                     onChange={handleDeviceSelectChange}
                 >
                     <option value="DEFAULT"
-                            id="device_selector">{`Select device. ${devices.length} device detected`}</option>
+                        id="device_selector">{`Select device. ${devices.length} device detected`}</option>
                     {devices.map((device) => (
                         <option
                             key={device.serial}
@@ -55,6 +100,22 @@ const DeviceSelector: FC = () => {
                         </option>
                     ))}
                 </select>
+                {
+                    deviceUsed ?
+                        <div className="form-control">
+                            <label className="label cursor-pointer justify-start gap-2">
+                                <input
+                                    disabled={deviceUsed.state !== DeviceState.ONLINE}
+                                    type="checkbox"
+                                    className="checkbox checkbox-primary checkbox-xs md:checkbox-sm"
+                                    checked={autoSelectEnabled}
+                                    onChange={handleAutoSelectChange}
+                                />
+                                <span className="text-xs md:text-md">Auto select when open AndroModem</span>
+                            </label>
+                        </div>
+                        : <></>
+                }
             </div>
         </div>
     )
